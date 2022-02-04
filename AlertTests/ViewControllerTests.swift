c9526b60304623c5f4416699d85fe580bacba9ce
Tim Batty <tbatty7@gmail.com>
2022-02-04 16:59:11 -0500

    Moved viewController initialization to setup method

--- AlertTests/ViewControllerTests.swift
+++ AlertTests/ViewControllerTests.swift
@@ -4,10 +4,14 @@ import ViewControllerPresentationSpy
 
 final class ViewControllerTests: XCTestCase {
     private var alertVerifier: AlertVerifier!
+    private var viewController: ViewController!
     
     override func setUp() {
         super.setUp()
         alertVerifier = AlertVerifier()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        viewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
+        viewController.loadViewIfNeeded()
     }
     
     override func tearDown() {
@@ -15,11 +19,7 @@ final class ViewControllerTests: XCTestCase {
         super.tearDown()
     }
     
-    func test_tappingButton_shouldShowAlert() throws {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
-        viewController.loadViewIfNeeded()
-        
+    func test_tappingButton_shouldShowAlert() throws {       
         tap(viewController.button)
         alertVerifier.verify(title: "Do the THING", message: "Let us know if you want us to do the thing", animated: true, actions: [.cancel("Cancel"), .default("Ok"),], preferredStyle: .alert, presentingViewController: viewController)
         XCTAssertEqual(alertVerifier.preferredAction?.title, "Ok", "preferred action")
