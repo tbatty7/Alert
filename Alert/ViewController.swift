5c38168d26b278d3ae71a8ec847a4659c0d2b136
Tim Batty <tbatty7@gmail.com>
2022-02-04 16:06:26 -0500

    Added button that triggers an alert with 2 optional buttons you can click, Ok and Cancel

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private(set) var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction private func buttonTap() {
+        let alert = UIAlertController(title: "Do the THING", message: "Let us know if you want us to do the thing", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print(">>>> Cancel")
+        }
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            print(">>>> OK")
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        alert.preferredAction = okAction
+        present(alert, animated: true)
+    }
+    
 }
 
