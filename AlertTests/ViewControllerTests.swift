7b329566f0f10f4a97b0ad0c984ba47f5c7b67d6
Tim Batty <tbatty7@gmail.com>
2022-02-04 17:00:15 -0500

    added viewController to teardown

--- AlertTests/ViewControllerTests.swift
+++ AlertTests/ViewControllerTests.swift
@@ -16,10 +16,11 @@ final class ViewControllerTests: XCTestCase {
     
     override func tearDown() {
         alertVerifier = nil
+        viewController = nil
         super.tearDown()
     }
     
-    func test_tappingButton_shouldShowAlert() throws {       
+    func test_tappingButton_shouldShowAlert() throws {
         tap(viewController.button)
         alertVerifier.verify(title: "Do the THING", message: "Let us know if you want us to do the thing", animated: true, actions: [.cancel("Cancel"), .default("Ok"),], preferredStyle: .alert, presentingViewController: viewController)
         XCTAssertEqual(alertVerifier.preferredAction?.title, "Ok", "preferred action")
